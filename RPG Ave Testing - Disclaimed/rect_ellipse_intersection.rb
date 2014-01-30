dd190a0388de89e8096cba4c1e4bf84bf93dd5e7
wmichaelv <wmichaelv@hotmail.com>
2014-01-30 14:19:27 -0800

    efficiency check

--- RPG Ave Testing - Disclaimed/rect_ellipse_intersection.rb
+++ RPG Ave Testing - Disclaimed/rect_ellipse_intersection.rb
@@ -8,35 +8,43 @@ module Intersect
     
     "ellipse equation => (x-cx/rx)^2 + (y-cy/ry)^2 = 1"
     
-    "1 - (cx/rx)^2 = (cy/ry)^2"
+    "1 - ((x -cx)/rx)^2 = (cy/ry)^2"
     
-    "(1 - (cx/rx)^2)^.5 = cy/ry"
+    "(1 - ((x- cx)/rx)^2)^.5 = cy/ry"
     
-    "cy = ry * (1 - (cx/rx)^2)^.5"
+    "y = ry * (1 - (cx/rx)^2)^.5 + cy"
     
     cx_f = cx.to_f
-    
-    return ((rect.x + rect.width).between?(cx - rx, cx + rx) ||
-           ((rect.y + rect.height).between?(get_uy(rect.x + rect.width, cx_f, cy,rx, ry), 
-                                            get_ly(rect.x + rect.width, cx_f, cy, rx, ry)) ||
-            (rect.y - rect.height).between?(get_uy(rect.x + rect.width, cx_f, cy,rx, ry), 
-                                            get_ly(rect.x + rect.width, cx_f, cy, rx, ry)))) ||
-           ((rect.x - rect.width).between?(cx - rx, cx + rx) ||                                 
-           ((rect.y + rect.height).between?(get_uy(rect.x - rect.width, cx_f, cy,rx, ry), 
-                                            get_ly(rect.x - rect.width, cx_f, cy, rx, ry)) ||
-            (rect.y - rect.height).between?(get_uy(rect.x - rect.width, cx_f, cy,rx, ry), 
-                                            get_ly(rect.x - rect.width, cx_f, cy, rx, ry))));
-    
+      
+    if rect.x + rect.width > cx + rx ||
+       rect.x - rect.width < cx - rx ||
+       rect.y + rect.height > cy + ry ||
+       rect.y - rect.height < cy - ry
+       
+       return false
+      
+    else
+      
+      return ((rect.y + rect.height).between?(get_ly(rect.x + rect.width, cx_f, cy,rx, ry), 
+                                              get_uy(rect.x + rect.width, cx_f, cy, rx, ry)) ||
+              (rect.y - rect.height).between?(get_ly(rect.x + rect.width, cx_f, cy,rx, ry), 
+                                              get_uy(rect.x + rect.width, cx_f, cy, rx, ry))) ||                                
+             ((rect.y + rect.height).between?(get_ly(rect.x - rect.width, cx_f, cy,rx, ry), 
+                                              get_uy(rect.x - rect.width, cx_f, cy, rx, ry)) ||
+              (rect.y - rect.height).between?(get_ly(rect.x - rect.width, cx_f, cy,rx, ry), 
+                                              get_uy(rect.x - rect.width, cx_f, cy, rx, ry)));
+                                              
+    end
     
   end
   "f(x) upper divission"
   def self.get_uy(x, cx_f,cy,rx, ry)
-    return (ry * Math.sqrt((1 - (x - cx_f) / rx * (x - cx_f) / rx).abs.to_i).to_i) + cy
+    return ry * 1 - Math.sqrt(((x - cx_f) / rx * (x - cx_f) / rx).abs) + cy
   end
   
   "f(x) lower divission"
   def self.get_ly(x, cx_f, cy, rx, ry)
-    return (-ry * Math.sqrt((1 - (x - cx_f)/rx * (x - cx_f)/rx).abs.to_i).to_i) + cy
+    return -ry * 1 - Math.sqrt(((x - cx_f) / rx * (x - cx_f) / rx).abs) + cy
   end
   
 end
