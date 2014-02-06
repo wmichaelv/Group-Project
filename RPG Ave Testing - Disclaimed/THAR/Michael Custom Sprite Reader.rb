0a6c83f1ae5c364fc1a6a71220ed62684a6a1f08
wmichaelv <wmichaelv@hotmail.com>
2014-02-06 14:17:57 -0800

    Idle animation is attached to game_player

--- RPG Ave Testing - Disclaimed/THAR/Michael Custom Sprite Reader.rb
+++ RPG Ave Testing - Disclaimed/THAR/Michael Custom Sprite Reader.rb
@@ -99,17 +99,40 @@ module Cache
 
 end
 
+#==============================================================================
+# Sprite_Character
+#==============================================================================
 class Sprite_Character < Sprite_Base
 	def set_character_bitmap
     
-    if (@character.id == 1)
+    if @character.class == Game_Player
       
-      @counter += 1 unless @counter.nil?
-      @counter = 0 if @counter.nil?
-      self.bitmap = Cache.touhou(Touhou::FolderSources['Battler'][0][0], "stand%03d" % @counter)
-      self.x = (Graphics.width - bitmap.width) / 2
-      self.y = (Graphics.height - bitmap.height) / 2
-      
+      if (@character.actor.name == 'Alice')
+        
+        $m_action = 'stand' if $m_action.nil?
+        unless $m_counter.nil?
+          case $m_action
+          when 'stand'
+            $m_counter += 0.33
+            $m_counter = 0 if $m_counter > 15
+          end
+        end
+        $m_counter = 0 if $m_counter.nil?
+        self.bitmap = Cache.touhou(Touhou::FolderSources['Battler'][0][0], "#{$m_action}%03d" % $m_counter)
+        $m_x = (Graphics.width - bitmap.width) / 2 if $m_x.nil?
+        $m_y = (Graphics.height - bitmap.height - bitmap.height / 2) / 2 if $m_y.nil?
+        self.x = @character.screen_x - bitmap.width / 2
+        self.y = @character.screen_y - bitmap.height + 16
+        if self.x < 0
+          self.src_rect.x = -self.x
+          self.x = 0
+        end
+        if self.y < 0
+          self.src_rect.y = -self.y
+          self.y = 0
+        end
+      end
+    
     else
     
       self.bitmap = Cache.character(@character_name)
@@ -129,14 +152,31 @@ class Sprite_Character < Sprite_Base
   end
   
   def update_src_rect
-    
-    if (@character.id == 1)
-      unless @counter.nil?
-        @counter += 1
-        @counter = 0 if @counter > 15
+    if @character.class == Game_Player
+      if (@character.actor.name == 'Alice')
+        
+        $m_action = 'stand' if $m_action.nil?
+        unless $m_counter.nil?
+          case $m_action
+          when 'stand'
+            $m_counter += 0.33
+            $m_counter = 0 if $m_counter > 15
+          end
+        end
+        $m_counter = 0 if $m_counter.nil?
+        self.bitmap = Cache.touhou(Touhou::FolderSources['Battler'][0][0], "#{$m_action}%03d" % $m_counter)
+        self.x = @character.screen_x - bitmap.width / 2
+        self.y = @character.screen_y - bitmap.height + 16
+        if self.x < 0
+          self.src_rect.x = -self.x
+          self.x = 0
+        end
+        if self.y < 0
+          self.src_rect.y = -self.y
+          self.y = 0
+        end
       end
-      @counter = 0 if @counter.nil?
-      self.bitmap = Cache.touhou(Touhou::FolderSources['Battler'][0][0], "stand%03d" % @counter)
+      
     else
       
       if @tile_id == 0
@@ -151,9 +191,34 @@ class Sprite_Character < Sprite_Base
   end
   
   def update_position
-    
-    if (@character.id == 1)
-      
+    if @character.class == Game_Player
+      if (@character.actor.name == "Alice")
+        #$just_once ||= 1
+        #p "Its PASSING!!" if $just_once == 1 && $just_once += 1
+        case $game_player.direction
+        when 2
+          #down
+          if Input.trigger?(:DOWN) || Input.repeat?(:DOWN)
+            #$m_y += ($game_map.scroll_type == 2 || $game_map.scroll_type == 3) ? 32 : 0
+          end
+        when 4
+          #left
+          if Input.trigger?(:LEFT) || Input.repeat?(:LEFT)
+            #$m_x -= ($game_map.scroll_type == 1 || $game_map.scroll_type == 3) ? 32 : 0
+          end
+        when 6 
+          #right
+          if Input.trigger?(:RIGHT) || Input.repeat?(:RIGHT)
+            #$m_x += ($game_map.scroll_type == 1 || $game_map.scroll_type == 3) ? 32 : 0
+          end
+        when 8
+          #up
+          if Input.trigger?(:UP) || Input.repeat?(:UP)
+            #$m_y -= ($game_map.scroll_type == 2 || $game_map.scroll_type == 3) ? 32 : 0
+          end
+        end
+      end
+ 
     else
       
       move_animation(@character.screen_x - x, @character.screen_y - y)
@@ -166,4 +231,3 @@ class Sprite_Character < Sprite_Base
   end
   
 end
-
