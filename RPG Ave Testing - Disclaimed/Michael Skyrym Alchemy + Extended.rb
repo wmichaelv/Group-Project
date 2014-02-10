5e98ca74fbc2d27ab7408d155bf6a221d8afb3eb
wmichaelv <wmichaelv@hotmail.com>
2014-02-09 18:01:29 -0800

    V 0.11

--- RPG Ave Testing - Disclaimed/Michael Skyrym Alchemy + Extended.rb
+++ RPG Ave Testing - Disclaimed/Michael Skyrym Alchemy + Extended.rb
@@ -2,7 +2,7 @@
 #
 # Michael Skyrym Alchemy + Extended
 # Last Updated: 2014.02.08
-# V 0.1
+# V 0.11
 # Requirement: RPG Maker VX Ace
 #             -Knowledge of 'how to use script and notetag'
 #
@@ -14,7 +14,7 @@
 #==============================================================================
 # Compatibility
 #==============================================================================
-# - RPG::BaseItem is the only overloaded class, so compatibility
+# - RPG::BaseItem DataManager are the only overloaded class, so compatibility
 #   is pretty high.
 #
 #==============================================================================
@@ -78,15 +78,93 @@
 #==============================================================================
 
 #==============================================================================
+# Bio
+#==============================================================================
+#
+# 2014.02.09 - V 0.11  Game_Alchemy is made
+# 2014.02.08 - V 0.1   Script is initailized
+#
+#==============================================================================
+
+
+#==============================================================================
 # RPG::BaseItem
 #==============================================================================
 
 class RPG::BaseItem
+
   attr_accessor :cmake
+  attr_accessor :prod
+  attr_accessor :requiredType
+  attr_accessor :requiredAmmo
+  attr_accessor :ingr
+
   alias RPG_BaseItem_Initialize initialize
   def initialize; RPG_BaseItem_Initialize; @cmake = false end
-  def checkCanMake?; 
+
+  def checkCanMake?
+    setRequirement if !@requirementSet
+    updatePartyHas
+    
+  end
+
+  def setRequirement
+
+    @requiredType = Hash.new {|h,k| h[k]=[]}
+    @requiredAmmo = Hash.new {|h,k| h[k]=[]}
+    @prod = self.note.scan(/(?<=<Reqr:).*?(?=[(])/)
+    @ingr = self.note.scan(/(?<=<Type:).*?(?=[>])/)
+    @prod << self.note[/(?<=<Prod:).*?(?=[(])/]
+    @prod << self.note[/(?<=<Ingr:).*?(?=[>])/]
+    @requiredType[@prod] << self.note[/(?<=<Prod:).*?(?=[>])/][/(?<=[(]).*?(?=[,])/].to_i 
+    @requiredAmmo[@prod] << self.note[/(?<=<Prod:).*?(?=[>])/][/(?<=[,]).*?(?=[)])/].to_i
+    
+    @requirementSet = true
+    
+  end
+
+  def updatePartyHas
+
+
+  end
+
 end
+
+#==============================================================================
+# DataManager
+#==============================================================================
+
+module DataManager
+
+  alias michael_DataManager_create_game_objects create_game_objects
+  def self.create_game_objects
+
+    michael_DataManager_create_game_objects
+    $game_alchemy = Game_Alchemy.new
+
+  end
+
+end
+
+#==============================================================================
+# Game_Alchemy
+#==============================================================================
+
+class Game_Alchemy
+
+  attr_accessor :product
+
+  def initialize
+
+    @product = []
+    $data_items.each do |item| @product << $data_items[item] if $data_items[item].note.match(/<Prod/) end
+    $data_weapons.each do |item| @product << $data_weapons[item] if $data_weapons[item].note.match(/<Prod/) end
+    $data_armors.each do |item| @product << $data_armors[item] if $data_armors[item].note.match(/<Prod/) end
+    @product.each do |item| item.checkCanMake? end
+  end
+
+end
+
 
 #==============================================================================
 # Scene_Alchemy
@@ -169,18 +247,50 @@ class Window_Product < Window_Command
 
   attr_accessor :ingredient
 
-  def initialize(x, y)
-
-    super(x, y)
-    @product = []
-    $data_items.each do |item| @product << $data_items[item] if $data_items[item].note.match(/<Prod/) end
-    $data_weapons.each do |item| @product << $data_weapons[item] if $data_weapons[item].note.match(/<Prod/) end
-    $data_armors.each do |item| @product << $data_armors[item] if $data_armors[item].note.match(/<Prod/) end
-
+  def make_command_list
+    $game_alchemy.product.each do |i| add_command(i.name, :productList) end
   end
 
+  def window_width; 160 end
+  def window_height; 508 end
+  def item; $game_alchemy.product && index >= 0 ? $game_alchemy.product[index] : nil end
+  def select_last; select($game_alchemy.product.index($game_party.last_item.object) || 0) end
+  
+  def draw_item(index)
+
+    rect = item_rect(index)
+    rect.x += 4
+    rect.width -= 8
+    icon_index = $game_alchemy.product[index].icon_index
+    self.contents.clear_rect(rect)
+    if !icon_index.nil?
+      rect.x -= 4
+      draw_icon(icon_index, rect.x, rect.y, $game_alchemy.product[index].cmake)
+      rect.x += 26
+      rect.width -= 20
+    end
+    self.contents.clear_rect(rect)
+    self.contents.font.color = normal_color
+    self.contents.font.color.alpha = enabled ? 255 : 128
+    self.contents.draw_text(rect, $game_alchemy.product[index].name)
+  end
+
+  def update_help; @help_window.set_item(item) end
+  def refresh; super; checkEnable end
+  def checkEnable; $game_alchemy.product.each do |i| i.cmake = i.checkCanMake end
+end
+#==============================================================================
+# Window_Ingredient
+#==============================================================================
+=begin
+class Window_Product < Window_Command
+
+  attr_accessor :ingredient
+
+  def initialize(x, y)
+
   def make_command_list
-    @product.each do |i| add_command(i.name, :productList) end
+    $game_alchemy.product.each do |i| add_command(i.name, :productList) end
   end
 
   def window_width; 160 end
@@ -209,11 +319,9 @@ class Window_Product < Window_Command
   def refresh; super; checkEnable end
   def checkEnable
     @product.each do |i|
-end
-#==============================================================================
-# Window_Ingredient
-#==============================================================================
 
+end
+=end
 #==============================================================================
 # Window_Confirm
 #==============================================================================
