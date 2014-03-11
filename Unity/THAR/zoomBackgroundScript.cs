0ea3cd3d6d73d2d02760e6eb2aebdb872a77027b
wmichaelv <wmichaelv@hotmail.com>
2014-03-11 14:35:07 -0700

    Zoom Script for Background And Character Display

---
+++ Unity/THAR/zoomBackgroundScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zoomBackgroundScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (transform.localScale.x > 1F && transform.localScale.x < 4.9F)
+			transform.localScale += (new Vector3(1F, 1F, 0) * Input.GetAxis("Mouse ScrollWheel"));
+		if (transform.localScale.x < 1F)
+			transform.localScale = new Vector3 (1F, 1F, 0);
+		if (transform.localScale.x > 5F)
+			transform.localScale = new Vector3 (5F, 5F, 0);
+
+	}
+}
---
+++ Unity/THAR/zoomCameraScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zoomCameraScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Camera.main.orthographicSize -= 2F * Input.GetAxis("Mouse ScrollWheel");
+		if (Camera.main.orthographicSize < 2F)
+			Camera.main.orthographicSize = 2F;
+		if (Camera.main.orthographicSize > 10F)
+			Camera.main.orthographicSize = 10F;
+	}
+}
