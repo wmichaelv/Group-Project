80016144666340d8da98b21be2da3c6d58986b07
wmichaelv <wmichaelv@hotmail.com>
2014-03-26 18:34:53 -0700

    Set Up Background Scrolling

--- Unity/THAR/scrollBackgroundScript.cs
+++ Unity/THAR/scrollBackgroundScript.cs
@@ -3,13 +3,54 @@ using System.Collections;
 
 public class scrollBackgroundScript : MonoBehaviour {
 
+
+	
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (setMapScroll()) scrollMap(Alice_Movement.playerMovSpd);
 	}
+
+	public static bool setMapScroll() {
+		return true;
+	}
+
+	void scrollMap(float mS) {
+		switch(Alice_Movement.playerDirection) {
+		case 0: // Idle
+			transform.Translate(new Vector2(0,0) * mS * Time.deltaTime);
+			return;
+		case 1: // Down-Left
+			transform.Translate(new Vector2(1,1) * mS * Time.deltaTime);
+			return;
+		case 2: // Down
+			transform.Translate(new Vector2(0,1) * mS * Time.deltaTime);
+			return;
+		case 3: // Down-Right
+			transform.Translate(new Vector2(-1,1) * mS * Time.deltaTime);
+			return;
+		case 4: // Left
+			transform.Translate(new Vector2(1,0) * mS * Time.deltaTime);
+			return;
+		case 6: // Right
+			transform.Translate(new Vector2(-1,0) * mS * Time.deltaTime);
+			return;
+		case 7: // Up-Left
+			transform.Translate(new Vector2(1,-1) * mS * Time.deltaTime);
+			return;
+		case 8: // Up
+			transform.Translate(new Vector2(0,-1) * mS * Time.deltaTime);
+			return;
+		case 9: // Up-Right
+			transform.Translate(new Vector2(-1,-1) * mS * Time.deltaTime);
+			return;
+		default:
+			transform.Translate(new Vector2(0,0) * mS * Time.deltaTime);
+			return;
+		}
+	}
+
 }
