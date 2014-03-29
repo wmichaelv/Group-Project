a8a7cbe2619db96ea4254e81d052010c0237bdb1
wmichaelv <wmichaelv@hotmail.com>
2014-03-28 19:49:53 -0700

    [On Hold] scrollBackground vs scrollCamera

--- Unity/THAR/scrollBackgroundScript.cs
+++ Unity/THAR/scrollBackgroundScript.cs
@@ -3,12 +3,32 @@ using System.Collections;
 
 public class scrollBackgroundScript : MonoBehaviour {
 
-
+	public static float topMapBorder;    // Store topMapBorder     to Pass Around Scripts
+	public static float leftMapBorder;   // Store leftMapBorder    to Pass Around Scripts
+	public static float bottomMapBorder; // Store bottomMapBorder  to Pass Around Scripts
+	public static float rightMapBorder;  // Store rightMapBorder   to Pass Around Scripts
 	
 	// Use this for initialization
 	void Start () {
+
+		setMapBorder();
 	}
-	
+
+	void setMapBorder() {
+
+		bottomMapBorder = transform.Find("BottomRightCorner").transform.position.y;
+		rightMapBorder = transform.Find("BottomRightCorner").transform.position.x;
+		topMapBorder = transform.Find("TopLeftCorner").transform.position.y;
+		leftMapBorder = transform.Find("TopLeftCorner").transform.position.x;
+
+		Debug.Log(bottomMapBorder);
+		Debug.Log(rightMapBorder);
+		Debug.Log(topMapBorder);
+		Debug.Log(leftMapBorder);
+	}
+
+
+
 	// Update is called once per frame
 	void Update () {
 		if (setMapScroll()) scrollMap(Alice_Movement.playerMovSpd);
