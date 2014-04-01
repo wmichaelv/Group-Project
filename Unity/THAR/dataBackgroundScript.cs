fa6fd6765965843c69e5a18a53dff080e0cf8473
wmichaelv <wmichaelv@hotmail.com>
2014-04-01 10:23:09 -0700

    Switching from Scrolling Map to Scrolling Camera

---
+++ Unity/THAR/dataBackgroundScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dataBackgroundScript : MonoBehaviour {
+
+	public static float topMapBorder;    // Store topMapBorder     to Pass Around Scripts
+	public static float leftMapBorder;   // Store leftMapBorder    to Pass Around Scripts
+	public static float bottomMapBorder; // Store bottomMapBorder  to Pass Around Scripts
+	public static float rightMapBorder;  // Store rightMapBorder   to Pass Around Scripts
+	public static float mapX;            // Store map x-coordinate to Pass Around Scripts
+	public static float mapY;            // Store map y-coordinate to Pass Around Scripts
+
+	// Use this for initialization
+	
+	void Start () {
+
+		setMapBorder();
+	}
+
+	void setMapBorder() {
+
+		bottomMapBorder = transform.Find("BottomRightCorner").transform.position.y;
+		rightMapBorder = transform.Find("BottomRightCorner").transform.position.x;
+		topMapBorder = transform.Find("TopLeftCorner").transform.position.y;
+		leftMapBorder = transform.Find("TopLeftCorner").transform.position.x;
+
+		Debug.Log(bottomMapBorder);
+		Debug.Log(rightMapBorder);
+		Debug.Log(topMapBorder);
+		Debug.Log(leftMapBorder);
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		mapX = transform.position.x;
+		mapY = transform.position.y;
+
+	}
+}
