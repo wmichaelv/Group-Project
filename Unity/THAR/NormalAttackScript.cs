e4bbd9286adcc5ea25813201183c1a80415a0114
wmichaelv <wmichaelv@hotmail.com>
2014-04-05 10:41:27 -0700

    Adjust Y Rotation Axis

--- Unity/THAR/NormalAttackScript.cs
+++ Unity/THAR/NormalAttackScript.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 public class NormalAttackScript : MonoBehaviour {
 
-	private Vector2 objectMouse; // Store mouse position based on character 
-	private bool hasReversed;
+	private Vector2 objectMouse; // Store mouse position based on character
+	private bool rotatedX;
+	private bool rotatedY;
 	private float prevRotate;
 	private float currentRotate;
-	private float storeGetRotation;
+	private float storeGetRotationX;
+	private float storeGetRotationY;
 	
 	// Use this for initialization
 	void Start () {
-		hasReversed = false;
+		rotatedX = false;
+		rotatedY = false;
 		currentRotate = 0;
 		prevRotate = 0;
 	
@@ -23,25 +26,28 @@ public class NormalAttackScript : MonoBehaviour {
 		//Debug.Log("prev    = " + prevRotate);
 
 		objectMouse = Alice_Movement.mousePos;
-		storeGetRotation = getRotation();
+		storeGetRotationX = getRotationX();
+		storeGetRotationY = getRotationY(Alice_Movement.playerReverse);
 
 		//Debug.Log("current = " + currentRotate);
-		//Debug.Log("rotate  = " + storeGetRotation);
+		//Debug.Log("rotate  = " + storeGetRotationX);
 
 		//Rotate All Child Objects
 		foreach (Transform child in transform) {
-			child.transform.Rotate(storeGetRotation, 0, 0);
+			child.transform.Rotate(storeGetRotationX, storeGetRotationY, 0);
+			child.particleSystem.startRotation = currentRotate * Mathf.Deg2Rad;
 		}
 	}
 
-	float getRotation() {
-		currentRotate =  -Mathf.Atan2(objectMouse.y, objectMouse.x) *  Mathf.Rad2Deg;
+
+	float getRotationX() {
+		currentRotate =  -Mathf.Atan2(objectMouse.y, objectMouse.x) * Mathf.Rad2Deg;
 
 		if (prevRotate == currentRotate) {
-			if (hasReversed) {
+			if (rotatedX) {
 				return 0;
 			} else {
-				hasReversed = true;
+				rotatedX = true;
 				return currentRotate;
 			}
 		} else {
@@ -53,6 +59,25 @@ public class NormalAttackScript : MonoBehaviour {
 			prevRotate = currentRotate;
 			return currentRotate;
 		}
+	}
+
+	float getRotationY(bool r) {
+
+		if (r)
+			if (rotatedY) {
+				return 0;
+			} else {
+				rotatedY = true;
+				return -180;
+			}
+		else
+			if (rotatedY) {
+				rotatedY = false;
+				return -180;
+			} else {
+				return 0;
+			}
+
 	}
 
 }
