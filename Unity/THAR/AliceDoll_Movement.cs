1ad1147c6a4a806b9953801fed74e1959dc000a4
wmichaelv <wmichaelv@hotmail.com>
2014-04-14 18:11:57 -0700

    Particle System Mouse Control

--- Unity/THAR/AliceDoll_Movement.cs
+++ Unity/THAR/AliceDoll_Movement.cs
@@ -1,30 +1,81 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class AliceDoll_Movement : MonoBehaviour {
 
 	private Animator animator;    // Store Animator
+	private Vector2 objectMouse;
+	private bool rotatedY;
+	private bool dollAction;
+	private float prevRotate;
+	private float currentRotate;
+	//private bool activateNormal;
+	private float timer;
 	//private bool dollAction; // Store dollAction
 
 	// Use this for initialization
 	void Start () {
-
 		animator = this.GetComponent<Animator>();
-	
+		prevRotate = 0;
+		currentRotate = 0;
+		//activateNormal = true;
+		rotatedY = false;
+		timer = 0;
 	}
 	
 	// Update is call once per turn
 	void Update() {
 
-		//dollAction = getDollAction();
+		objectMouse = playerControlScript.mousePos;
+		animator.transform.Rotate(0,getRotationY(Alice_Movement.charaReverse), getRotationZ());
+
+		dollAction = getDollAction();
+
+
+		if (dollAction) {
+			timer += (1f / 60);
+			if (timer > .5) {
+				timer = 0f;
+				Global.inherentParticleEmission(transform.Find("Normal Attack").gameObject);
+			}
+		}
 
 		setDollMovState();
 		// setMovementAnimationState
 
 	}
 
+	float getRotationY(bool r) {
+		if (r)
+			if (rotatedY) {
+				return 0;
+			} else {
+				rotatedY = true;
+				return -180;
+			}
+		else
+			if (rotatedY) {
+				rotatedY = false;
+				return -180;
+			} else {
+				return 0;
+			}
+	}
+
+	float getRotationZ() {
+		currentRotate = Mathf.Atan2(objectMouse.y, objectMouse.x) * Mathf.Rad2Deg;
+
+		if (prevRotate == currentRotate) {
+			return 0;
+		} else {
+			animator.transform.Rotate(0,0,-prevRotate);
+			prevRotate = currentRotate;
+			return currentRotate;
+		}
+	}
+
 	bool getDollAction() {
-		return false;
+		return Input.GetMouseButton(0);
 	}
 
 	void setDollMovState() {
@@ -39,25 +90,15 @@ public class AliceDoll_Movement : MonoBehaviour {
 	}
 
 	void setDollNonActionMovState() {
-		// Check if Character is idle
-		//if (DollDirection == 0) setIdleMovState();
-		//else 
 		setDollMovingMovState();
 	}
 
 	void setDollMovingMovState() {
-		// Set if Sprite needs to be reversed
-		//animator.transform.Rotate(0, getRotation(DollReverse), 0);
-		// Check if Character is moving to position of mouse
 		setIdleMovState();
-		//if (DollMouse) setDollForwardMovState();
-		//else setDollBackwardMovState();
 	}
 
 	void setIdleMovState() {
-		// isDashing() is to finish pre#, post# dashing animation
-		//if (!isDashing()) 
 		animator.Play("AliceDollStand");
 	}
-
+	
 }
